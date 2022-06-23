5d99f30417f8cf4227dbb1eb2f9388d3fba7097d
Cleonildo Jr <cleu.junior@gmail.com>
2022-06-22 23:15:32 -0300

    first commit

---
+++ src/main/java/com/bootcamp/devsup/TddEventCityApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.devsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddEventCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddEventCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/devsup/TddEventCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.devsup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddEventCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
